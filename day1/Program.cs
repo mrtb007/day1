2da327f89871e0bdcf94fc7c9937f2a2b0a58ee4
BOSS ALICE <BOSS ALICE@LAPTOP-HKQLA6SI>
2022-09-12 20:42:13 +0700

    Add project files.

---
+++ day1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace day1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //string userName = "Nguyen Ngoc Thinh";
+            //string tuoi = "32";
+            //int tuoi1 = Convert.ToInt32(tuoi);
+
+            //string profile = "Ten : " + userName+ ", Tuoi : " + tuoi1;
+            // Ten : Nguyen Ngoc Thinh, Tuoi : 32.
+
+            // call 1 class 
+            var lession = new lession1();
+            Console.WriteLine(lession.intro2());
+
+        }
+    }
+}
---
+++ day1/lession1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day1
+{
+    public class lession1
+    {
+
+        private string intro()
+        {
+            string userName = "Nguyen Ngoc Thinh";
+            string tuoi = "32";
+            int tuoi1 = Convert.ToInt32(tuoi);
+
+            string profile = "Ten : " + userName + ", Tuoi : " + tuoi1;
+
+            return profile;
+        }
+
+        public string intro2()
+        {
+            return intro();
+        }
+    }
+}
