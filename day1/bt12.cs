7b81c033031e692632525c38ecaad869b282d538
nguyen thinh <mrtb2020@outlook.com>
2022-09-13 23:00:16 +0700

    bt12

---
+++ day1/bt12.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//5.Write a program in C# Sharp to display the cube of the number up to given an integer. Go to the editor
+//Test Data :
+//Input number of terms : 5
+//Expected Output :
+//Number is : 1 and cube of the 1 is :1
+//Number is : 2 and cube of the 2 is :8
+//Number is : 3 and cube of the 3 is :27
+//Number is : 4 and cube of the 4 is :64
+//Number is : 5 and cube of the 5 is :125
+namespace day1
+{
+    public class bt12
+    {
+        public static void b12()
+        {
+            int num;
+            Console.Write("nhap so bat ky: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < num+1; i++)
+                Console.WriteLine($"{i} ^ 3 = {i*i*i}");
+        }
+    }
+}
